e0614cecde7076c01de236c199b686513c267367
Donovan <oswaldo.d.b.e@hotmail.com>
2022-07-08 11:42:14 -0500

    connect livewire & form

--- resources/views/livewire/crear-vacante.blade.php
+++ resources/views/livewire/crear-vacante.blade.php
@@ -2,13 +2,13 @@
     <div class="">
         <x-label for="titulo" :value="__('Titulo Vacante')" />
 
-        <x-input id="titulo" class="block mt-1 w-full" type="text" name="titulo" :value="old('titulo')" placeholder="Titulo Vacante"/>
+        <x-input id="titulo" class="block mt-1 w-full" type="text" wire:model="titulo" :value="old('titulo')" placeholder="Titulo Vacante"/>
     </div>
 
     <div class="">
         <x-label for="salario" :value="__('Salario Mensual')" />
 
-        <select name="salario" id="salario" class="rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 w-full">
+        <select wire:model="salario" id="salario" class="rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 w-full">
             <option value="">-- Seleccionar --</option>
             @foreach ($salarios as $salario)
                 <option value="{{ $salario->id }}">{{ $salario->salario }}</option>
@@ -19,7 +19,7 @@
     <div class="">
         <x-label for="categoria" :value="__('Categoria')" />
 
-        <select name="categoria" id="categoria" class="rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 w-full">
+        <select wire:model="categoria" id="categoria" class="rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 w-full">
             <option value="">-- Seleccionar --</option>
             @foreach ($categorias as $categoria)
                 <option value="{{ $categoria->id }}">{{ $categoria->categoria }}</option>
@@ -30,25 +30,25 @@
     <div class="">
         <x-label for="empresa" :value="__('Empresa')" />
 
-        <x-input id="empresa" class="block mt-1 w-full" type="text" name="empresa" :value="old('empresa')" placeholder="Empresa ej. Netflix, Uber, Microsoft"/>
+        <x-input id="empresa" class="block mt-1 w-full" type="text" wire:model="empresa" :value="old('empresa')" placeholder="Empresa ej. Netflix, Uber, Microsoft"/>
     </div>
 
     <div class="">
         <x-label for="ultimo_dia" :value="__('Último día para postularse')" />
 
-        <x-input id="ultimo_dia" class="block mt-1 w-full" type="date" name="ultimo_dia" :value="old('ultimo_dia')"/>
+        <x-input id="ultimo_dia" class="block mt-1 w-full" type="date" wire:model="ultimo_dia" :value="old('ultimo_dia')"/>
     </div>
 
     <div class="">
         <x-label for="descripcion" :value="__('Descripción Puesto')" />
 
-        <textarea name="descripcion" id="descripcion" class="rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 w-full h-72" placeholder="Descripción general del puesto, experiencia..."></textarea>
+        <textarea wire:model="descripcion" id="descripcion" class="rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 w-full h-72" placeholder="Descripción general del puesto, experiencia..."></textarea>
     </div>
 
     <div class="">
         <x-label for="imagen" :value="__('Imagen Vacante')" />
 
-        <x-input id="imagen" class="block mt-1 w-full" type="file" name="imagen"/>
+        <x-input id="imagen" class="block mt-1 w-full" type="file" wire:model="imagen"/>
     </div>
 
     <x-button>
