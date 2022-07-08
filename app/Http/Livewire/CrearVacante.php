44b6d37c611c9514c14cfa291dd623ef94295b99
Donovan <oswaldo.d.b.e@hotmail.com>
2022-07-08 12:38:31 -0500

    save image

--- app/Http/Livewire/CrearVacante.php
+++ app/Http/Livewire/CrearVacante.php
@@ -33,6 +33,17 @@ class CrearVacante extends Component
     public function crearVacante()
     {
         $datos = $this->validate();
+
+        // Almacenar la imagen
+        $imagen = $this->imagen->store('public/vacantes');
+        $nombre_imagen = str_replace('public/vacantes/', '', $imagen);
+
+        // Crear la vacante
+
+        // Crear un mensaje
+
+        // Redireccionar al usuario
+
     }
 
     public function render()
