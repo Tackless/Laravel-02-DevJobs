7ce5b6e94818395568cdd275c0d813a8680d8774
Donovan <oswaldo.d.b.e@hotmail.com>
2022-07-10 17:15:57 -0500

    hide create & edit vacantes to devs

--- app/Http/Controllers/VacanteController.php
+++ app/Http/Controllers/VacanteController.php
@@ -15,6 +15,8 @@ class VacanteController extends Controller
     public function index()
     {
         //
+        $this->authorize('viewAny', Vacante::class);
+
         return view('vacantes.index');
     }
 
@@ -26,6 +28,8 @@ class VacanteController extends Controller
     public function create()
     {
         //
+        $this->authorize('create', Vacante::class);
+
         return view('vacantes.create');
     }
 
