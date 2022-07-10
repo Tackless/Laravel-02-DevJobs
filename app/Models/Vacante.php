2b9f1c849f52b55a6b825c34bc2cd6a475278e77
Donovan <oswaldo.d.b.e@hotmail.com>
2022-07-10 16:35:07 -0500

    add function to get categoria & salario

--- app/Models/Vacante.php
+++ app/Models/Vacante.php
@@ -2,8 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Categoria;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Vacante extends Model
 {
@@ -21,4 +22,14 @@ class Vacante extends Model
         'imagen',
         'user_id',
     ];
+
+    public function categoria()
+    {
+        return $this->belongsTo(Categoria::class);
+    }
+    
+    public function salario()
+    {
+        return $this->belongsTo(Salario::class);
+    }
 }
